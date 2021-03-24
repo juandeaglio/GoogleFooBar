f292dd0fa11386d39726f83dc29e7a40fb986e12
juandeaglio <45726108+juandeaglio@users.noreply.github.com>
2021-03-23 17:30:33 -0700

    Created Alphabet class which should map braille to letters of alphabet

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,35 @@
 package com.company;
 
-public class Main {
+import java.util.Dictionary;
 
+public class Main
+{
+    public static class Alphabet
+    {
+        final int SIZE_OF_LETTER = 6;
+        Dictionary<String,String> englishToBrailleDictionary;
+        String plainAlphabet = "The quick brown fox jumps over the lazy dog";
+        String binaryAlphabet = "000001011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100011100000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110";
+        public Alphabet()
+        {
+            mapDictionaryFromPlainToBinary(plainAlphabet, binaryAlphabet);
+        }
+        void mapDictionaryFromPlainToBinary(String plain, String binary)
+        {
+            String pText = plain;
+            String bText = binary;
+            while(binaryAlphabet.length() > 0 && plainAlphabet.length() > 0)
+            {
+
+                if (Character.isUpperCase(pText.charAt(0)))
+                {
+                    String binaryLetter = bText.substring(0,6);
+                    englishToBrailleDictionary.put(" ", binaryLetter);
+                    bText = bText.substring(6);
+                }
+            }
+        }
+    }
     public static String main(String plaintext)
     {
         return TranslateEnglishToBraille(plaintext);
@@ -18,3 +46,4 @@ public class Main {
         return "";
     }
 }
+
