aa89f63aa0f3458724bf6de803070f7196b79383
juandeaglio <45726108+juandeaglio@users.noreply.github.com>
2021-03-23 18:17:24 -0700

    Created a Translator class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,15 @@ import java.util.Map;
 
 public class Main
 {
-
     public static String main(String plaintext)
     {
         String plainAlphabet = "The quick brown fox jumps over the lazy dog";
         String binaryAlphabet = "000001011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100011100000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110";
-        return TranslateEnglishToBraille(plaintext);
-    }
-
-    private static String TranslateEnglishToBraille(String plaintext) {
-        String translation = "";
-        translation = Translate(plaintext);
-        return translation;
-    }
-
-    public static String Translate(String plaintext)
-    {
+        Alphabet alphabet = new Alphabet(plainAlphabet, binaryAlphabet);
+        Translator translator = new Translator(alphabet);
         return "";
     }
+
+
+
 }
---
+++ src/com/company/Translator.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Translator
+{
+    Alphabet myAlphabet;
+    public Translator(Alphabet alphabet)
+    {
+        myAlphabet = alphabet;
+    }
+    public String TranslateEnglishToBraille(String plaintext)
+    {
+        String translation = "";
+        return translation;
+    }
+}
