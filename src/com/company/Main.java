419abb9fe8279056df6742fd700b1d9f8efca92b
juandeaglio <45726108+juandeaglio@users.noreply.github.com>
2021-03-23 17:10:56 -0700

    Initial commit and initial test set up

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,19 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static String main(String plaintext)
+    {
+        return TranslateEnglishToBraille(plaintext);
+    }
+
+    private static String TranslateEnglishToBraille(String plaintext) {
+        String translation = "";
+        translation = Translate(plaintext);
+        return translation;
+    }
+
+    public static String Translate(String plaintext)
+    {
+        return "";
     }
 }
